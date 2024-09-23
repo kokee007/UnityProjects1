36c092c53d8af1a15dee547abba42d39a4a1d375
kokee007 <alexalvarez@ceroca.cat>
2024-09-23 16:23:24 +0200

    NauControlCantonades

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -6,10 +6,23 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private float _vel;
 
+
+    private Vector2 minPantalla, maxPantalla;
+
     // Start is called before the first frame update
     void Start()
     {
-        _vel = 8; 
+        _vel = 8;
+
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        minPantalla.x = minPantalla.x + 0.75f;
+        maxPantalla.x = maxPantalla.x - 0.75f;
+
+        minPantalla.y = minPantalla.y + 0.75f;
+        maxPantalla.y = maxPantalla.y - 0.75f;
+
     }
 
     // Update is called once per frame
@@ -24,6 +37,11 @@ public class NewBehaviourScript : MonoBehaviour
 
         Vector2 novaPos = transform.position;
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
+        
+
         transform.position = novaPos;
     }
 }
